186d15f81db8f7e9fe2902841d68717b1c5f7e53
Muhammad Ayaz <ayaznaseerkhan171@gmail.com>
2025-08-22 22:54:04 +0500

    the invoice initilization error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:pos/core/services/hive_service.dart';
 import 'package:pos/features/Home/Services/dashboard_service.dart';
+import 'package:pos/features/auth/screens/welcome_screen.dart';
 import 'package:pos/features/navigation/screens/navigation_screen.dart';
+import 'package:pos/core/services/hive_service.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await HiveService.init();
-  await DashboardService().initialize(); // ✅ ab pehle hi init ho jayega
-  runApp(const MyApp());
+  try {
+    await HiveService.init(); // Initialize Hive & all boxes
+    await DashboardService().initialize(); // Initialize DashboardService
+    runApp(const MyApp());
+  } catch (e) {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "⚠️ Error initializing app data.\n\n$e",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +41,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'POS App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const NavigationScreen(),
+      title: 'AURA POS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/', // 👈 Default route
+      routes: {
+        '/': (context) => const WelcomeScreen(),
+        '/main': (context) => const NavigationScreen(),
+      },
     );
   }
 }
